9c83e4cde0639c5ea72585055f28c600bda01311
Emiliano Zuchovicki <emilianozuchovicki@hotmail.com>
2020-09-29 07:21:47 -0500

    Add Comments , Finish project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_190005) do
+ActiveRecord::Schema.define(version: 2020_09_18_205429) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
